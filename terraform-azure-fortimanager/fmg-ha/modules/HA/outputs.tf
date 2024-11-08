d311712c2e1c0949a6700afbc9cb51e48e75ede9
yarafe <yarafe@fortinet.com>
2024-11-08 09:58:44 +0100

    u

--- terraform-azure-fortimanager/fmg-ha/modules/HA/outputs.tf
+++ terraform-azure-fortimanager/fmg-ha/modules/HA/outputs.tf
@@ -17,7 +17,7 @@ output "deployment_summary" {
     fmg1_private_ip_address = azurerm_network_interface.fmg1ifc.private_ip_address
 	fmg2_public_ip_address  = data.azurerm_public_ip.fmg2pip.ip_address
     fmg2_private_ip_address = azurerm_network_interface.fmg2ifc.private_ip_address
-	ha_public_ip_address  = data.azurerm_public_ip.hapip.ip_address
+	ha_public_ip_address  =  var.ha_ip == "public" && length(data.azurerm_public_ip.hapip) > 0 ? data.azurerm_public_ip.hapip[0].ip_address : null
   })
 }
 
@@ -40,10 +40,12 @@ output "fmg2_public_ip_address" {
 }
 
 data "azurerm_public_ip" "hapip" {
-  name                = azurerm_public_ip.hapip.name
+  count               = var.ha_ip == "public" ? 1 : 0
+  name                = var.ha_ip == "public" ? azurerm_public_ip.hapip[0].name : ""
   resource_group_name = var.resource_group_name
 }
 
 output "ha_public_ip_address" {
-  value = data.azurerm_public_ip.hapip.ip_address
+  value = var.ha_ip == "public" && length(data.azurerm_public_ip.hapip) > 0 ? data.azurerm_public_ip.hapip[0].ip_address : null
+}
 }
