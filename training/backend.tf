ca3b461f8ead8d9ac5f01232dd7fb0fa61fbd20b
yarafe <yarafe@fortinet.com>
2025-03-20 16:51:10 +0100

    u

--- training/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "External-Training-Utility"
-    storage_account_name = "terraform4training"
-    container_name       = "terraformstate"
-	key                  = "${var.StorageKey}"
-  }
-}
